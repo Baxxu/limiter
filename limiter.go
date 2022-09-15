f4b034e0f8f4da8da8d59f059fff0cc9ebaf0644
Ivan <baxxulol@gmail.com>
2022-09-15 10:43:07 +0300

    queue added

--- limiter.go
+++ limiter.go
@@ -1,22 +1,27 @@
 package limiter
 
-type Limiter struct {
-	limiter chan struct{}
-}
+type Limiter chan struct{}
 
 func New(maxGoroutinesCount int) Limiter {
-	return Limiter{
-		limiter: make(chan struct{}, maxGoroutinesCount),
-	}
+	return make(chan struct{}, maxGoroutinesCount)
 }
 
 func (limiter *Limiter) Add() {
-	limiter.limiter <- struct{}{}
+	*limiter <- struct{}{}
 }
 
 func (limiter *Limiter) Remove() {
 	select {
-	case <-limiter.limiter:
+	case <-*limiter:
 	default:
 	}
+}
+
+// Queue starts func in goroutine. If goroutines limit reached, wait till some goroutines released
+func (limiter *Limiter) Queue(callback func()) {
+	limiter.Add()
+	go func() {
+		defer limiter.Remove()
+		callback()
+	}()
 }
